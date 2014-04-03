90d220f35afeee3e6016d79754490111cb2b95d0
Nick Burne <nickburne@users.noreply.github.com>
2014-04-03 09:04:47 +0100

    Update version number

--- pootlepress-apple-menu.php
+++ pootlepress-apple-menu.php
@@ -3,7 +3,7 @@
 Plugin Name: Canvas Extension - Papple Menu
 Plugin URI: http://pootlepress.com/
 Description: An extension for WooThemes Canvas that contains a menu design inspired by apple.com. This helps you customise the look and feel of your navigation menu in the Canvas theme by WooThemes.
-Version: 1.0.1
+Version: 1.0.2
 Author: PootlePress
 Author URI: http://pootlepress.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -16,6 +16,6 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
 	require_once( 'classes/class-pootlepress-apple-menu.php' );
 
     $GLOBALS['pootlepress_apple_menu'] = new Pootlepress_Apple_Menu( __FILE__ );
-    $GLOBALS['pootlepress_apple_menu']->version = '1.0.1';
+    $GLOBALS['pootlepress_apple_menu']->version = '1.0.2';
 	
 ?>
